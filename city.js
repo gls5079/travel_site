f9c42801a8b329e599ad9c50010ef89cdd956bca
gls5079 <32943697+gls5079@users.noreply.github.com>
2019-05-29 19:36:36 -0700

    Update city.js
    Deleted unnecessary comments

--- city.js
+++ city.js
@@ -76,7 +76,7 @@ module.exports = function(){
 				res.write(JSON.stringify(error));
 				res.end();
 			}else{
-				res.redirect('/city'); //Should this be /explore-cities.html?
+				res.redirect('/city');
 			}
 		});
 	});
