4cb64fbdeec2f282cb2d7af457661e0dcb717bd3
gls5079 <32943697+gls5079@users.noreply.github.com>
2019-06-01 11:19:53 -0700

    Update airlineprice.js

--- airlineprice.js
+++ airlineprice.js
@@ -13,6 +13,20 @@ module.exports = function(){
 			complete();
 		});
 	}
+	
+	//function to select one airline price information in order to update it
+	function getAirlinePrice(res, mysql, context, id, complete){
+		var sql = "SELECT ap.id, ap.starting_city_id, ap.ending_city_id, ap.flight_date, ap.price FROM Airline_Price ap WHERE ap.id=?";
+		var inserts = [id];
+		mysql.pool.query(sql, inserts, function(error, results, fields){
+			if(error){
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			context.airline_price = results[0];
+			complete();  
+		});
+	}
 
 	//function to select city info
 	function getCities(res, mysql, context, complete){
@@ -53,6 +67,43 @@ module.exports = function(){
 				res.render('airlineprice', context);
 			}
 		}
+	});
+	
+	//Displays a single airline price in order to update the airline price attributes
+	router.get('/:id', function(req, res){
+		var callbackCount = 0;
+		var context = {};
+		context.jsscripts = ["select.js", "update.js"];
+		var mysql = req.app.get('mysql');
+		getAirlinePrice(res, mysql, context, req.params.id, complete);
+		getCities(res, mysql, context, complete);
+		getAirlines(res, mysql, context, complete);
+		function complete(){
+			callbackCount++;
+			if(callbackCount >= 3){
+				res.render('update-airlineprice', context);
+			}
+
+		}
+	});
+	
+	//Sends updated attributes and redirects to the airline price page
+	router.put('/:id', function(req, res){
+		var mysql = req.app.get('mysql');
+		console.log(req.body)
+		console.log(req.params.id)
+		var sql = "UPDATE Airline_Price ap SET ap.starting_city_id=?, ap.ending_city_id=?, ap.flight_date=?, ap.price=? WHERE ap.id=?";
+		var inserts = [req.body.starting_city_id, req.body.ending_city_id, req.body.flight_date, req.body.price, req.params.id];
+		sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+			if(error){
+				console.log(error)
+				res.write(JSON.stringify(error));
+				res.end();
+			}else{
+				res.status(200);
+				res.end();
+			}
+		});
 	});
 	
 	//add price then redirects back to airline price page
