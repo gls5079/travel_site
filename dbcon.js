d1d3cc1a6a0aa889153cb2330a385daeb36f5d6f
gls5079 <32943697+gls5079@users.noreply.github.com>
2019-06-08 22:57:13 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -4,6 +4,7 @@ var pool = mysql.createPool({
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_framet',
   password        : '8365',
-  database        : 'cs340_framet'
+  database        : 'cs340_framet',
+  dateStrings     : 'date'
 });
 module.exports.pool = pool;
