e650e3b61fdbe11c8e7e7c15bf506de274cc50ed
gls5079 <32943697+gls5079@users.noreply.github.com>
2019-06-11 16:23:32 -0700

    Update destinationtype.js

--- destinationtype.js
+++ destinationtype.js
@@ -14,6 +14,20 @@ module.exports = function(){
 			complete();
 		});
 	}
+	
+	//function to select one city's destination information in order to update it
+	function getDestinationType(res, mysql, context, id, complete){
+		var sql = "SELECT dt.id, c.name, dt.city_id, dt.type FROM Destination_Type dt INNER JOIN City c ON dt.city_id=c.id WHERE dt.id=?";
+		var inserts = [id];
+		mysql.pool.query(sql, inserts, function(error, results, fields){
+			if(error){
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			context.destination_type = results[0];
+			complete();  
+		});
+	}
 
 	//function to select city information
 	function getCities(res, mysql, context, complete){
@@ -41,6 +55,42 @@ module.exports = function(){
 				res.render('destinationtype', context);
 			}
 		}
+	});
+	
+	//Displays a single city's destination type in order to update the destination type attribute
+	router.get('/:id', function(req, res){
+		var callbackCount = 0;
+		var context = {};
+		context.jsscripts = ["update.js"];
+		var mysql = req.app.get('mysql');
+		getDestinationType(res, mysql, context, req.params.id, complete);
+		getCities(res, mysql, context, complete);
+		function complete(){
+			callbackCount++;
+			if(callbackCount >= 2){
+				res.render('update-destinationtype', context);
+			}
+
+		}
+	});
+	
+	//Sends updated type attribute and redirects to the destination type page
+	router.put('/:id', function(req, res){
+		var mysql = req.app.get('mysql');
+		console.log(req.body)
+		console.log(req.params.id)
+		var sql = "UPDATE Destination_Type dt SET dt.type=? WHERE dt.id=?";
+		var inserts = [req.body.type, req.params.id];
+		sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+			if(error){
+				console.log(error)
+				res.write(JSON.stringify(error));
+				res.end();
+			}else{
+				res.status(200);
+				res.end();
+			}
+		});
 	});
 	
 	/* Adds a destination type to a city, then redirects back to the destination type page*/
